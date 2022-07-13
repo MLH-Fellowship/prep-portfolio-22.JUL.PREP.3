0b1f87b2a0217487ef0b8bcc75c084e0e56af2c8
gibbonhug <71813760+gibbonhug@users.noreply.github.com>
2022-07-13 16:07:33 -0400

    Add more pins, reduce radius of pins

--- assets/js/world-map.js
+++ assets/js/world-map.js
@@ -1,24 +1,24 @@
 'use strict';
 
-(function() {
+(function () {
+    console.log('script loaded');
+
     // if updating default color also have to update
     // the world-map.html file (the #pins fill)
     const PIN_COLOR_DEFAULT = '#133667';
     const PIN_COLOR_HOVER = 'white';
 
-    console.log("script loaded");
+    let pins = Array.from(document.querySelectorAll('.pin'));
 
-    let pin = document.querySelector('.pin'); // only 1 pin so far
+    pins.forEach((pin) => {
+        // mouseover: change color for now
+        pin.addEventListener('mouseover', () => {
+            pin.style.fill = PIN_COLOR_HOVER;
+        });
 
-
-    // mouseover: change color for now
-    pin.addEventListener('mouseover', (evt) => {
-        pin.style.fill = PIN_COLOR_HOVER;
-    })
-
-    // mouseout: reset hover color
-    pin.addEventListener('mouseout', (evt) => {
-        pin.style.fill = PIN_COLOR_DEFAULT;
-    })
-
+        // mouseout: reset hover color
+        pin.addEventListener('mouseout', () => {
+            pin.style.fill = PIN_COLOR_DEFAULT;
+        });
+    });
 })();
