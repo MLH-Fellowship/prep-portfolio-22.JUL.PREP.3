e01903073efbca667fdaf2dfb87d38bbb75dc5b5
gibbonhug <71813760+gibbonhug@users.noreply.github.com>
2022-07-13 15:55:16 -0400

    Event listeners for mouseover and mouseout now change color of pin

--- assets/js/world-map.js
+++ assets/js/world-map.js
@@ -1,11 +1,24 @@
 'use strict';
 
 (function() {
+    // if updating default color also have to update
+    // the world-map.html file (the #pins fill)
+    const PIN_COLOR_DEFAULT = '#133667';
+    const PIN_COLOR_HOVER = 'white';
+
     console.log("script loaded");
 
     let pin = document.querySelector('.pin'); // only 1 pin so far
 
+
+    // mouseover: change color for now
     pin.addEventListener('mouseover', (evt) => {
-        console.log(evt.target.toString());
+        pin.style.fill = PIN_COLOR_HOVER;
     })
+
+    // mouseout: reset hover color
+    pin.addEventListener('mouseout', (evt) => {
+        pin.style.fill = PIN_COLOR_DEFAULT;
+    })
+
 })();
