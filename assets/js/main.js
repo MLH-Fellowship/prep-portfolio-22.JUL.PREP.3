4999e6eea731498ecaeafa1dc638ce6139db091f
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2022-07-14 19:41:47 +0400

    Create auto switcher for system time sync

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,17 @@ checkbox.addEventListener("change", () => {
 
   localStorage.setItem("darkMode", switchToTheme);
 });
+
+// The mode switch automatically on page load
+function autoSwitch() {
+  var hour = (new Date()).getHours();
+  if (hour >= "9" && hour <= "17") {
+    localStorage.setItem("darkMode", "light");
+  } else {
+    localStorage.setItem("darkMode", "dark");
+  }
+}
+
+if(localStorage.getItem("darkMode") === null) {
+  window.onload = autoSwitch;
+}
