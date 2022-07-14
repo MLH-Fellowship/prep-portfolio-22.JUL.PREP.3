32525e5a31747b918be3991dbd21b4c3a270dfd9
Di Wu <dw2761@nyu.edu>
2022-07-14 01:15:32 -0400

    Dark mode for card, layout changed for button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
-const checkbox = document.getElementById("checkbox");
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.documentElement.setAttribute("data-theme", "dark");
+});
+
 
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+
+  // Switch between `dark` and `light`
+  var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+  // Set our currenet theme to the new one
+  document.documentElement.setAttribute("data-theme", switchToTheme);
 });
