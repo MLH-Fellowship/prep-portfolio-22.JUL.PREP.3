516c3513cc023dcd2c1b8917dc2e91e391762c3d
Di Wu <dw2761@nyu.edu>
2022-07-16 04:01:14 -0400

    Applied dark mode to leader board table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
 let checkbox = document.getElementById("checkbox");
 
-document.addEventListener("DOMContentLoaded", function (event) {
+$(document).ready(function (event) {
   // Set the default mode to light
   if (localStorage.getItem("darkMode") === null) {
     window.onload = autoSwitch;
-    
+
   } else {
     if (localStorage.getItem("darkMode") === "dark") {
       document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("checkStatus", false);
     } else {
       document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("checkStatus", true);
     }
-    if(localStorage.getItem("checkStatus") != null){
-      checkbox.checked = localStorage.getItem("checkStatus");
-    }
-    
+  }
+  if(localStorage.getItem("checkStatus") != null){
+    checkbox.checked = localStorage.getItem("checkStatus");
   }
 });
 
@@ -25,12 +26,17 @@ checkbox.addEventListener("change", () => {
   // Switch between `dark` and `light`
   var switchToTheme = currentTheme === "dark" ? "light" : "dark";
 
+  let isChecked = checkbox.checked;
+
+  console.log(isChecked);
+
   // Set our currenet theme to the new one
   document.documentElement.setAttribute("data-theme", switchToTheme);
 
   localStorage.setItem("darkMode", switchToTheme);
 
-  localStorage.setItem("checkStatus", checkbox.checked);
+  localStorage.setItem("checkStatus", isChecked);
+
 });
 
 // The mode switch automatically on page load
@@ -39,9 +45,13 @@ function autoSwitch() {
   if (hour >= "9" && hour <= "17") {
     localStorage.setItem("darkMode", "light");
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("checkStatus", true);
+    checkbox.checked = true;
   } else {
     localStorage.setItem("darkMode", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("checkStatus", false);
+    checkbox.checked = false;
   }
 }
 
